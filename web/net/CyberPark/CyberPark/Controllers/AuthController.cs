71042ccbd3b012d6b8a559b79fb258a11a9ca258
Alexei Kojenov <akojenov@salesforce.com>
2020-01-18 22:45:57 -0800

    .net: comments

--- web/net/CyberPark/CyberPark/Controllers/AuthController.cs
+++ web/net/CyberPark/CyberPark/Controllers/AuthController.cs
@@ -16,24 +16,32 @@ namespace CyberPark.Controllers
   {
     public IEnumerable<string> GetAllAuth()
     {
+      // use binary formatter for serialization
       BinaryFormatter formatter = new BinaryFormatter();
 
+      // is there Authorization header?
       if (Request.Headers.Contains("Authorization"))
       {
+        // get the header's value and base64-decode it
         byte[] authBytes = Convert.FromBase64String(Request.Headers.GetValues("Authorization").Single());
-
         var memoryStream = new MemoryStream(authBytes);
-        string[] dummy = (string []) formatter.Deserialize(memoryStream); // totally safe LOL
+
+        // totally safe LOL
+        string[] dummy = (string[])formatter.Deserialize(memoryStream);
 
         return dummy;
       }
+
       else
       {
-        string[] dummy = new string[] { "the", "best" };
-
+        // just return something to the user
+        string[] dummy = new string[] { "foo", "bar" };
         var memoryStream = new MemoryStream();
+
+        // serialize dummy object
         formatter.Serialize(memoryStream, dummy);
 
+        // base64-encode the serialized data and return
         return new string[] { "Sample Authorization value", Convert.ToBase64String(memoryStream.ToArray()) };
       }
 
